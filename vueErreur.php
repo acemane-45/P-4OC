cc1db062a1c446d869cff98de7327e542544675b
acemane-45 <58085788+acemane-45@users.noreply.github.com>
2019-12-03 18:18:51 +0100

    Delete vueErreur.php

--- vueErreur.php
+++
@@ -1,7 +0,0 @@
-<?php $titre = 'Blog de Jean Forteroche'; ?>
-
-<?php ob_start() ?>
-<p>Une erreur est survenue : <?= $msgErreur ?></p><!-- message en cas d'erreur-->
-<?php $contenu = ob_get_clean(); ?>
-
-<?php require 'gabarit.php'; ?>
